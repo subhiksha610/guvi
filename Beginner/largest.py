d98e4c29ab2c8a3ae8c644f0d5ee12ef8662e573
subhiksha610 <53897661+subhiksha610@users.noreply.github.com>
2019-08-10 11:55:15 +0530

    Update largest.py

--- Beginner/largest.py
+++ Beginner/largest.py
@@ -1,12 +1,4 @@
-num1 = float(input(" "))
-num2 = float(input(" "))
-num3 = float(input(" "))
-if(num1>num2)and(num1>num3):
-   largest = num1
-   print(largest)
-elif(num2>num1)and(num2>num3):
-   largest = num2
-   print(largest)
-else:
-    largest = num3
-    print(largest)
+x=input(" ")
+y=input(" ")
+Z=input(" ")
+print(max(x,y,Z))
